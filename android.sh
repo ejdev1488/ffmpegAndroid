786afef46ee11c0274f219bf6ff8b958d272d9b3
ejdev1488 <131073045+ejdev1488@users.noreply.github.com>
2025-09-03 22:24:37 +0800

    Update android.sh

--- android.sh
+++ android.sh
@@ -174,10 +174,7 @@ configure_ffmpeg(){
 
    make clean
    make -j2
-   make install -j2
-   # Copy monolithic ffmpeg binary to output
-   cp "$FFMPEG_SOURCE_DIR/ffmpeg" "$PREFIX/ffmpeg"
-   
+   make install -j2  
 }
 
 echo -e "\e[1;32mCompiling FFMPEG for Android...\e[0m"
